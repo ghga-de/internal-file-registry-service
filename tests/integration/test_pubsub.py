a8b401d3456e0be8dff1f899b9b76983ba012050
Moritz Hahn <Moritz.Hahn@uni-tuebingen.de>
2022-01-19 18:08:12 +0000

    Removed grouping label from message

--- tests/integration/test_pubsub.py
+++ tests/integration/test_pubsub.py
@@ -84,7 +84,7 @@ def test_subscribe_stage_requests(psql_fixture, s3_fixture, amqp_fixture):  # no
     sub_and_pub_test_generic(
         upstream_topic_name=DEFAULT_CONFIG.topic_name_stage_request,
         downstream_topic_name=DEFAULT_CONFIG.topic_name_staged_to_outbox,
-        upstream_message=state.FILES["in_registry"].message,
+        upstream_message=state.FILES["no_grouping_label_in_message"].message,
         upstream_msg_schema=schemas.SCHEMAS["non_staged_file_requested"],
         downstream_msg_schema=schemas.SCHEMAS["file_staged_for_download"],
         subscribe_func=subscribe_stage_requests,
